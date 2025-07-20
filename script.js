58cb32d9674264e26b3b47ff3789a620c6438563
Richard Liu <richardadesigner@gmail.com>
2025-07-20 02:15:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -42,16 +42,6 @@ document.getElementById("copy").addEventListener("click", function () {
     navigator.clipboard.writeText(document.getElementById("display").value)
 })
 window.onload = function () {
-    fetch("https://573f616c-2f33-4a25-8dd3-eab903401ca9-00-15qkzj0ppjof8.spock.replit.dev/calc?q=6")
-        .then(function (responde) {
-            console.log("Server is running");
-        })
-        .catch(function (error) {
-            throw new Error("Server might be in sleep mode or stopped running");
-        })
-        .catch(function (error) {
-            alert(error)
-        })
     if (window.location.search) {
         var input = new URLSearchParams(window.location.search)
         document.getElementById("input").value = input.get("q")
