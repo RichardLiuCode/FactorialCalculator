c6cb838070914ba12aa764c370ce9ec9adf31707
Richard Liu <richardadesigner@gmail.com>
2025-07-20 09:54:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -47,4 +47,11 @@ window.onload = function () {
         document.getElementById("input").value = input.get("q")
         document.getElementById("enter").click()
     }
+     fetch("https://573f616c-2f33-4a25-8dd3-eab903401ca9-00-15qkzj0ppjof8.spock.replit.dev/calc?q=6")
+        .then(function (responde) {
+            console.log("✅ Render Server for Factorial Calculator App is running");
+        })
+        .catch(function (error) {
+            throw new Error("🚫 Render Server for Factorial Calculator App might be in sleep mode or stopped running");
+        })
 }
